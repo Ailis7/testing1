010ac0f4d07c94398548ba46b165f09b3decd2be
Petr Brovkin <bropet@mail.ru>
2020-08-26 13:32:15 +0300

    add test, update package

---
+++ src/js/__tests__/cardValidate.test.js
@@ -0,0 +1,79 @@
+import puppetteer from 'puppeteer';
+
+jest.setTimeout(30000); // default puppeteer timeout
+describe('card validate', () => {
+  let browser = null;
+  let page = null;
+  const baseUrl = 'http://localhost:8080';
+  beforeAll(async () => {
+    browser = await puppetteer.launch({
+      headless: false, // show gui
+      slowMo: 100,
+      devtools: true, // show devTools
+    });
+    page = await browser.newPage();
+  });
+  afterAll(async () => {
+    await browser.close();
+  });
+  describe('validate work', () => {
+    test('should add .validing class for valid inn', async () => {
+      await page.goto(baseUrl);
+      const main = await page.$('.main');
+      const input = await main.$('.numbers');
+      await input.type('4242424242424242');
+      const submit = await main.$('.click');
+      submit.click();
+      await page.waitForSelector('.validing');
+    });
+    test('not validing', async () => {
+      await page.goto(baseUrl);
+      const main = await page.$('.main');
+      const input = await main.$('.numbers');
+      await input.type('555');
+      const submit = await main.$('.click');
+      submit.click();
+      await page.waitForSelector('.notValiding');
+    });
+    test('Visa', async () => {
+      await page.goto(baseUrl);
+      const main = await page.$('.main');
+      const input = await main.$('.numbers');
+      await input.type('424');
+      await page.waitFor(() => {
+        const parent = document.querySelector('.visa').parentNode;
+        if (!parent.classList.contains('dark')) {
+          return parent;
+        }
+        return '';
+      });
+    });
+    test('maestro', async () => {
+      await page.goto(baseUrl);
+      const main = await page.$('.main');
+      const input = await main.$('.numbers');
+      await input.type('6777');
+      await page.waitFor(() => {
+        const parent = document.querySelector('.maestro').parentNode;
+        if (!parent.classList.contains('dark')) {
+          return parent;
+        }
+        return '';
+      });
+    });
+    test('maestro return dark', async () => {
+      await page.goto(baseUrl);
+      const main = await page.$('.main');
+      const input = await main.$('.numbers');
+      await input.type('62');
+      await page.keyboard.press('Backspace');
+      await page.waitFor(() => {
+        const parent = document.querySelector('.maestro').parentNode;
+        if (parent.classList.contains('dark')) {
+          return parent;
+        }
+        return '';
+      });
+    });
+  });
+});
--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,6 @@ import Listners from './classes/Listners';
 const cardCheck = new Listners();
 cardCheck.numListner();
 cardCheck.validating();
+
+const a = document.querySelector('.visa').parentNode;
+console.log(a);
